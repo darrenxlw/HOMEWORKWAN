9659631954afee14427ba43de7f3b08f5e5687e8
dxlw <dxlwan88@gmail.com>
2017-10-15 01:34:10 +0200

    Implemented draw_a_name

--- R/draw_a_name.R
+++ R/draw_a_name.R
@@ -1,15 +1,32 @@
-#' Plot the development of first names in France
+#' Draw the development of first names in France since 1900
 #'
-#' @param name
+#' @param the_name a character
+#' @param the_sex a character
 #'
+#' @import prenoms
 #' @import dplyr
+#' @import ggplot2
+#' @import ggthemes
+#' @import assertthat
 #'
-#' @return
+#' @return The plot of names
 #' @export
 #'
 #' @examples
+#' draw_a_name("Vincent", "M")
 #'
 
-draw_a_name <- function(name){
-  return(iris)
+draw_a_name <- function(the_name, the_sex){
+  assert_that(the_sex %in% c("M", "F"))
+  assert_that(is.string(the_name))
+
+  tmp <- prenoms::prenoms %>%
+    filter(sex==the_sex, name==the_name) %>%
+    group_by(year,sex,name) %>%
+    summarize(n=sum(n)) %>%
+    ggplot(aes(x=year,y=n)) +
+      geom_line() +
+      labs(title=the_name) +
+      theme_gdocs()
+  return(tmp)
 }
