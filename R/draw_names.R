a6987f080759bfcff74c11220d67cdd91cd253b6
darrenxlw <dxlwan88@gmail.com>
2017-10-15 03:54:28 +0200

    Completing draw_names function

--- R/draw_names.R
+++ R/draw_names.R
@@ -1,11 +1,35 @@
-#' Title
+#' Draw the development of multiple first names in France since 1900
 #'
-#' @param name_vector
+#' @param name_vector vector of names to be drawn
 #'
-#' @return
+#' @import prenoms
+#' @import dplyr
+#' @import ggplot2
+#' @import ggthemes
+#' @import assertthat
+#'
+#' @return The plot of names
 #' @export
 #'
 #' @examples
-draw_names <- function(name_vector){
+#' draw_names(c("Diane","Vincent"))
 
+draw_names <- function(name_vector){
+  assert_that(is.character(name_vector))
+
+  tmp <- prenoms::prenoms %>%
+    filter(name %in% name_vector) %>%
+    group_by(year,name) %>%
+    summarize(n=sum(n)) %>%
+    ggplot(aes(x=year,y=n, color=name)) +
+    geom_line() +
+    theme_gdocs()
+  # tmp <- prenoms::prenoms %>%
+  #   filter(name %in% name_vector) %>%
+  #   group_by(year,name) %>%
+  #   summarize(n=sum(n)) %>%
+  #   spread(key=name,value=n) %>%
+  #   dygraph()
+
+  return(tmp)
 }
