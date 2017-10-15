4987084a68ca27f3c5bb5b2aee0c955b8ed9a895
darrenxlw <dxlwan88@gmail.com>
2017-10-15 04:32:02 +0200

    my_csv_reader added

---
+++ R/my_csv_reader.R
@@ -0,0 +1,22 @@
+#' Reading all csv files in a directory into a list
+#'
+#' @param dir_path directoy where the csv files are read
+#'
+#' @import assertthat
+#'
+#' @return a list of dataframes made up of csv
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' my_csv_reader("..")
+#' }
+
+my_csv_reader <- function(dir_path=getwd()){
+  assert_that(is.dir(dirname(dir_path)))
+  assert_that(is.readable(dirname(getwd())))
+
+  csvs <- list.files(path=dir_path, pattern="*.csv$",full.names = TRUE)
+  csv_list <- lapply(csvs, read.csv)
+  return(csv_list)
+}
