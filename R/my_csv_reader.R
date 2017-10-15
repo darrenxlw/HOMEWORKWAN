c70bd6b4a5bec5fe7694694ee4d3bf2c3ceb7ec3
darrenxlw <dxlwan88@gmail.com>
2017-10-15 05:13:02 +0200

    Completed multi_excel and save_as_csv

--- R/my_csv_reader.R
+++ R/my_csv_reader.R
@@ -14,9 +14,9 @@
 
 my_csv_reader <- function(dir_path=getwd()){
   assert_that(is.dir(dirname(dir_path)))
-  assert_that(is.readable(dirname(getwd())))
+  assert_that(is.readable(dirname(dir_path)))
 
-  csvs <- list.files(path=dir_path, pattern="*.csv$",full.names = TRUE)
+  csvs <- list.files(path=dir_path, pattern="*\\.csv$",full.names = TRUE)
   csv_list <- lapply(csvs, read.csv)
   return(csv_list)
 }
